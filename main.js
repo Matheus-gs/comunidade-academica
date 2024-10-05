bf010877af2e7a941ac48321842866b6e8c9af75
matheus-gs <mgomes494@gmail.com>
2024-10-05 15:40:36 -0300

    feat: test reading data from github repository to fill the page.

--- main.js
+++ main.js
@@ -1,38 +1,27 @@
-console.log('👀')
-document
-.querySelector(".github-icon")
-.addEventListener("click", function () {
+const owner = "Matheus-gs";
+const repo = "comunidade-academica";
+const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/`;
+const linksContainer = document.getElementById("links");
+
+document.addEventListener("DOMContentLoaded", () => {
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (Array.isArray(data)) {
+        data.forEach((file) => {
+          const linkCard = document.createElement("a");
+          linkCard.className = "link-card";
+          linkCard.href = file.href;
+          linkCard.textContent = file.name;
+          linksContainer.appendChild(linkCard);
+        });
+      }
+    });
+});
+
+document.querySelector(".github-icon").addEventListener("click", function () {
   const link = document.createElement("a");
   link.href = "https://github.com/Matheus-gs/comunidade-academica";
   link.target = "_blank";
   link.click();
-});
-
-const linkList = [
-{
-  label: "Agenda de Eventos",
-  href: "https://google.com/search?q=eventos+do+udf",
-},
-{
-  label: "Notificações",
-  href: "https://google.com/search?q=notificacoes+udf",
-},
-{
-  label: "Espaço para Networking",
-  href: "https://google.com/search?q=networking+udf",
-},
-{
-  label: "Recursos Educacionais",
-  href: "https://google.com/search?q=recursos+udf",
-},
-];
-
-const linksContainer = document.getElementById("links");
-
-linkList.forEach((link) => {
-const linkCard = document.createElement("a");
-linkCard.className = "link-card";
-linkCard.href = link.href;
-linkCard.textContent = link.label;
-linksContainer.appendChild(linkCard);
 });
